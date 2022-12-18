fcd1eea368742ad0785d00aea006f1e220b8b1b1
parthchopra <parthchopra18@gmail.com>
2022-12-18 17:21:53 -0500

    Add unit tests

---
+++ BethanysPieShop.Tests/Controllers/PieControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BethanysPieShop.Controllers;
+using BethanysPieShop.Tests.Mocks;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Tests.Controllers
+{
+	public class PieControllerTests
+	{
+        [Fact]
+        public void List_EmptyCategory_ReturnsAllPies()
+        {
+            //arrange
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+
+            var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+            //act
+            var result = pieController.List("");
+
+            //assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
+
